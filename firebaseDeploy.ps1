d86b43693772d0eb4aca423b9059c7b93f602633
Florian Buergi <florian@buergi.cloud>
2023-06-03 01:02:21 +0200

    update app config to match live domain

--- firebaseDeploy.ps1
+++ firebaseDeploy.ps1
@@ -1,4 +1,6 @@
-$maps_api_key = Get-Content .\maps-api.key -First 1
-((Get-Content src\index.html) -replace '%MAPS_KEY_PLACEHOLDER%', $maps_api_key) | Out-File -encoding utf8 index.html
-ng build --configuration = production
-firebase deploy
+$index_file_content=Get-Content src\index.html
+$maps_api_key=Get-Content .\maps-api.key -First 1
+($index_file_content -replace '%MAPS_KEY_PLACEHOLDER%', $maps_api_key) | Out-File -encoding utf8 src\index.html
+ng build --configuration=production
+#firebase deploy
+$index_file_content | Out-File -encoding utf8 src\index.html
